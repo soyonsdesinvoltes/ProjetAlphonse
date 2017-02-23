d7376006cced9cd483103b90291086c9a41736d5
soyonsdesinvoltes <cornierebertrand@gmail.com>
2017-02-24 00:14:57 +0100

    chevalier

--- Chevalier.java
+++ Chevalier.java
@@ -4,7 +4,7 @@ package application;
 
 Participants au projet : Mathieu, Karine, Lucien et Bertrand
 
-Alphonse doit se dï¿½placer dans un labyrinthe, on rï¿½cupï¿½re le labyrinthe par un fichier texte dongeon.txt
+Alphonse doit se déplacer dans un labyrinthe, on récupére le labyrinthe par un fichier texte dongeon.txt
 
 
 */
@@ -12,12 +12,12 @@ Alphonse doit se dï¿½placer dans un labyrinthe, on rï¿½cupï¿½re le labyrinthe pa
 class Chevalier {
 private int x = 1; // position d'Alphonse en horizontal
 private int y = 1; // position d'Alphonse en vertical
-private char case_courante; // la case oï¿½ Alphonse se trouve, elle change ï¿½ chaque
-// dï¿½placement
-private char case_precedente; // la case oï¿½ Alphonse se trouvait, elle change ï¿½
-// chaque dï¿½placement
+private char case_courante; // la case où Alphonse se trouve, elle change à chaque
+// déplacement
+private char case_precedente; // la case où Alphonse se trouvait, elle change à
+// chaque déplacement
 private String direction = "Droite"; // la direction vers laquelle vas Alphonse,
-// elle est mise ï¿½ jour si besoin
+// elle est mise à jour si besoin
 
 
 // Constructeur
@@ -33,6 +33,7 @@ public Chevalier(int x,int y,char case_courante){
 
 /**
  * Modifie l'abscisse d'Alphonse
+ *
  * @param entier  x
  * @retour vide
  */
@@ -41,7 +42,7 @@ private void setX(int x) {
 }
 
 /**
- * Modifie l'ordonnï¿½e d'Alphonse
+ * Modifie l'ordonnée d'Alphonse
  *
  * @param entier y
  * @retour vide //
@@ -62,7 +63,7 @@ public void setCaseCourante(char case_courante) {
 }
 
 /**
- * Modifie la case prï¿½cï¿½dente d'Alphonse
+ * Modifie la case précédente d'Alphonse
  *
  * @param char
  *            case_precedente
@@ -84,7 +85,7 @@ private void setDirection(String direction) {
 }
 
 /**
- * Recupï¿½re l'abscisse d'Alphonse
+ * Recupère l'abscisse d'Alphonse
  *
  * @param vide
  * @retour entier $x abscisse du point
@@ -94,17 +95,17 @@ public int getX() {
 }
 
 /**
- * Recupï¿½re l'ordonnï¿½e d'Alphonse
+ * Recupère l'ordonnée d'Alphonse
  *
  * @param vide
- * @retour entier y ordonnï¿½e d'Alphonse
+ * @retour entier y ordonnée d'Alphonse
  */
 public int getY() {
     return this.y;
 }
 
 /**
- * Recupï¿½re la case courante d'Alphonse
+ * Recupère la case courante d'Alphonse
  *
  * @param vide
  * @retour char case_courante
@@ -114,7 +115,7 @@ public char getCaseCourante() {
 }
 
 /**
- * Recupï¿½re la case prï¿½cï¿½dente d'Alphonse
+ * Recupère la case précédente d'Alphonse
  *
  * @param vide
  * @retour char case_precedente
@@ -124,7 +125,7 @@ public char getCasePrecedente() {
 }
 
 /**
- * Recupï¿½re la direction d'Alphonse
+ * Recupère la direction d'Alphonse
  *
  * @param entier
  *            $x abscisse du point
@@ -135,7 +136,7 @@ public String getDirection() {
 }
 
 /**
- * Determine le premier dï¿½placement d'Alphonse en fonction de la
+ * Determine le premier déplacement d'Alphonse en fonction de la
  * configuration du labyrinthe,
  *
  * @param vide
@@ -143,25 +144,25 @@ public String getDirection() {
  */
 public void determinerDeplacementDepart(char[][] dongeon) {
     String deplacement = "";
-    char case_courante = getCaseCourante(); // Recupï¿½re la case courante
+    char case_courante = getCaseCourante(); // Recupère la case courante
     // d'Alphonse
 
-    if (case_courante == 'F') // teste si la case de dï¿½part est F ou E
+    if (case_courante == 'F') // teste si la case de départ est F ou E
     {
         deplacement = "A"; // Alphonse avance
-        x++; // Comme la position de dï¿½part est (1,1) nous definissons
-        // l'abscisse d'Alphonse ï¿½ x=2
-        setCasePrecedente('F'); // Definit la case de dï¿½part
+        x++; // Comme la position de départ est (1,1) nous definissons
+        // l'abscisse d'Alphonse à x=2
+        setCasePrecedente('F'); // Definit la case de départ
         setDirection("Droite");
     } else {
         deplacement = "DA"; // Alphonse doit pivoter vers le bas si la case
         // est E
-        y++; // Comme la position de dï¿½part est (1,1) nous definissons
-        // l'ordonnï¿½e d'Alphonse ï¿½ y=2
+        y++; // Comme la position de départ est (1,1) nous definissons
+        // l'ordonnée d'Alphonse à y=2
         setDirection("Bas"); // Change la direction d'Alphonse
     }
 
-    setCaseCourante(dongeon[x][y]); // Alphonse s'est dï¿½placï¿½ nous
+    setCaseCourante(dongeon[x][y]); // Alphonse s'est déplacé nous
     // changeons la case courante
     System.out.print("" + deplacement);
 }
@@ -193,16 +194,16 @@ public void toutDroit(char[][] dongeon){
         default:
             break;
     }
-    setCaseCourante(dongeon[x][y]); // Alphonse s'est dï¿½placï¿½ nous changeons la case courante
+    setCaseCourante(dongeon[x][y]); // Alphonse s'est déplacé nous changeons la case courante
     
 
 
 }
 
 /**
- * fonction DemiTour Alphonse est arrivï¿½ dans un cul de sac, il doit faire
+ * fonction DemiTour Alphonse est arrivé dans un cul de sac, il doit faire
  * demi tour. elle modifie la case courante, la case_precedente, la
- * direction et les coordonnï¿½es d'Alphonse
+ * direction et les coordonnées d'Alphonse
  *
  * @param vide
  * @retour vide
@@ -233,9 +234,9 @@ public void demiTour(char[][] dongeon) {
 }
 
 /**
- * fonction Angle90 Alphonse est arrivï¿½ dans un coin, il doit tourner. elle
+ * fonction Angle90 Alphonse est arrivé dans un coin, il doit tourner. elle
  * modifie la case courante, la case_precedente, la direction et les
- * coordonnï¿½es d'Alphonse
+ * coordonnées d'Alphonse
  *
  * @param vide
  * @retour vide
@@ -296,9 +297,9 @@ public void angle90(char[][] dongeon) {
 }
 
 /**
- * fonction Carrefour Alphonse est arrivï¿½ ï¿½ un carrefour, il doit dï¿½cider
+ * fonction Carrefour Alphonse est arrivé à un carrefour, il doit décider
  * de sa direction. elle modifie la case courante, la case_precedente, la
- * direction et les coordonnï¿½es d'Alphonse
+ * direction et les coordonnées d'Alphonse
  *
  * @param vide
  * @retour vide
